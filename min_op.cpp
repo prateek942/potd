ab70e4e586ffff08ff536ab66d13aba908e61c26
Prateek Kumar Pandey <121096878+prateek942@users.noreply.github.com>
2023-01-29 22:59:10 +0530

    Create min_op.cpp

---
+++ min_op.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int solve(int a, int b) {
+        // code here
+          if(a==b)return 0;
+
+        int an=a&b;
+
+        if(an==a and an!=b)return 1;
+
+        else if(an!=a and an==b) return 1;
+
+        else return 2;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int a;
+        scanf("%d",&a);
+        
+        
+        int b;
+        scanf("%d",&b);
+        
+        Solution obj;
+        int res = obj.solve(a, b);
+        
+        printf("%d\n", res);
+        
+    }
+}
+
+// } Driver Code Ends
