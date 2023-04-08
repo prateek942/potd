30980ec01c5a49bf632a1bf878f50f0178415a85
Prateek Kumar Pandey <121096878+prateek942@users.noreply.github.com>
2023-04-08 15:51:40 +0530

    Create make array beautiful.cpp

---
+++ make array beautiful.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        //if adjacent are negative remove them
+        
+        
+        // if adjacent are of same sign they are beautiful
+        vector<int> ans; int i=0;
+        while(i<arr.size()){
+            if(ans.empty() or (ans.back()>=0) ==(arr[i]>=0))
+                ans.push_back(arr[i]);
+            else
+                ans.pop_back();
+            i++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> res = obj.makeBeautiful(arr);
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
